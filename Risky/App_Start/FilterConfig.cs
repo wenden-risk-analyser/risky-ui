2f68a3d129ef726713a66f3dd08ea48d01a5945d
dwendo <dwenden@gmail.com>
2016-02-11 19:44:44 +1100

    feat: initial app setup
    * including nuget packages but will probably remove them
    * still probably have a few files to remove

---
+++ Risky/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Risky
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Risky/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Risky.Startup))]
+namespace Risky
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
